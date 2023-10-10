cf40ac6cba1f9fa0b279211e4da9d41300277922
ariel lima <ariel_lima_oliveira@yahoo.com.br>
2023-10-10 18:59:24 +0100

    adjusted syntax

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,9 @@
-import ExpenseDate from "./ExpenseDate";
 import  './ExpenseItem.css'
 function ExpenseItem(props) {
 
   return (
     <div className='expense-item'>
-      <ExpenseDate></ExpenseDate>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
